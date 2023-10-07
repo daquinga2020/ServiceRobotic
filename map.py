5cc808b5d0f3f80b03d9da88b36492fdbe0799ed
Daniel <da.quinga.2020@alumnos.urjc.es>
2023-10-07 19:41:40 +0200

    Algoritmo BSA en proceso sin busqueda de punto de retorno
    Signed-off-by: Daniel <da.quinga.2020@alumnos.urjc.es>

--- map.py
+++ map.py
@@ -1,6 +1,7 @@
+import time
 import numpy as np
 import cv2
-import time
+import util
 
 def draw_grid(img, hight, width, size_cell_x, size_cell_y):
     
@@ -17,28 +18,87 @@ def draw_grid(img, hight, width, size_cell_x, size_cell_y):
 def check_obs(img, px, py, size_cell_x, size_cell_y):
     
     # Comprobar si en la celdilla en la que se encuentra hay un obstaculo
-    for i in range(px+1, px + size_cell_x): # se puede borrar el 1 si elimino el grid
-        for j in range(py+1, py + size_cell_y):
-            if img[i,j] == 0:
+    for j in range(py+1, py + size_cell_y): # se puede borrar el 1 si elimino el grid
+        for i in range(px+1, px + size_cell_x):
+            if img[j,i] == 0:
+                fill_cell(img, px, py, size_cell_x, size_cell_y, 0)
                 return 1 # Hay obstaculo
     return 0
 
 
-def save_cells_free(img, hight, width, size_cell_x, size_cell_y):
+def save_free_cells(img, hight, width, size_cell_x, size_cell_y):
     
     f_cells = []
     
-    for i in range(0, width, size_cell_x):
-        for j in range(0, hight, size_cell_y):
+    for j in range(0, hight, size_cell_y):
+        for i in range(0, width, size_cell_x):
             if check_obs(img, i, j, size_cell_x, size_cell_y) != 1:
                 f_cells = f_cells + [(i,j)]
 
     return f_cells
 
+def fill_cell(img, px, py, size_cell_x, size_cell_y, color):
+    
+    # Rellenar celda si ha pasado por ahi
+    max_x = px + size_cell_x + 1
+    max_y = py + size_cell_y + 1
+    
+    if max_x >= img.shape[1]:
+        max_x = img.shape[1]-2
+    if max_y >= img.shape[0]:
+        max_y = img.shape[0]-2
+
+    for j in range(py, max_y): # se puede borrar el 1 si elimino el grid
+        for i in range(px, max_x):
+            img[j][i] = color
+
+
+def find_cell(cells, px_find, py_find, size_cell_x, size_cell_y):
+    # Función para encontrar la celda a la que pertenecen las coordenadas
+    for cell in cells:
+        x_corner, y_corner = cell
+        if px_find >= x_corner and px_find < x_corner + size_cell_x and py_find >= y_corner and py_find < y_corner + size_cell_y:
+            cells.remove(cell)
+            return (x_corner, y_corner)
+
+    return None
+
+def get_celldirection(direction, size_cell_x, size_cell_y):
+    
+    dx = 0
+    dy = 0
+    
+    if direction == "North":
+        dy = -size_cell_y
+    elif direction == "South":
+        dy = size_cell_y
+    elif direction == "East":
+        dx = size_cell_x
+    else:
+        dx = -size_cell_x
+    
+    return (dx, dy)
+
+def get_neighbors_cells(cells, current_cell, size_cell_x, size_cell_y):
+    
+    current_x = current_cell[0]
+    current_y = current_cell[1]
+    
+    cost = 1
+    neighbors_cells = []
+    
+    for dir in ["North", "East", "South", "West"]:
+        dx, dy = get_celldirection(dir,  size_cell_x, size_cell_y)
+        nextx, nexty = int(current_x + dx), int(current_y + dy)
+        nextCell = (nextx, nexty)
+        if nextCell in cells:
+            neighbors_cells.append( ( nextCell, dir, cost) )
+    
+    return neighbors_cells
 
 map_img = cv2.imread('mapgrannyannie.png', 0) # Leer la imagen
 h, w = map_img.shape
-# IMAGEN ALTURA: 1012 ANCHO 1013
+# IMAGEN ALTURA: 1012 ANCHO 1013; img[fila][columna]
 # cv2.imshow('ORIGINAL MAP', map_img)
 
 # First step: apply erosion function on the map image to dilate obstacles on the map
@@ -59,25 +119,135 @@ y0_rob = -1
 yaw0_rob = -3.77
 
 # Utilizar celdillas de 32x32 o 36x36, con 28 parece rellenar bastantes sitios
-cell_x = 28
-cell_y = 28
+cell_x = 18
+cell_y = 18
 
 draw_grid(map_erosion, h, w, cell_x, cell_y)
 
 # Guardar las celdillas libres fuera de obstaculos
 # Luego ubicar a nuestro robot en cual de esas celdillas esta.
-free_cells = save_cells_free(map_erosion, h, w, cell_x, cell_y)
+free_cells = save_free_cells(map_erosion, h, w, cell_x, cell_y)
 
-print("N:", len(free_cells), " CELDAS LIBRES: ", free_cells)
+# Luego ubicar a nuestro robot en cual de esas celdillas del mapa esta.
+# Para localizar al robot en el mapa se tomaran puntos conocidos en el mundo de Gazebo
+# y se relacionaran con los pixeles del mapa, posteriormente se emplea un script en matlab
+# con el que hallaremos un modelo de regresion lineal, obteniendo de esta manera la posicion del robot este donde este.
 
-x, y = free_cells[0]
+# Obtener la posicion del robot inicial
+color_visited = 220
+color_retrpnts = 100
+x0_rob = -1 # Si se avanza a la izquierda se suma en pos
+y0_rob = 1.5 # Si se avanza a la izquierda se suma en pos
+yaw0_rob = 0 #-3.77
+# 1290
 
-for i in range(x, x + 1 + cell_x): # se puede borrar el 1 si elimino el grid
-    for j in range(y, y + 1 + cell_y):
-        map_erosion[i][j] = 200
+# Prediccion obtenida del modelo de regresion lineal
+x_start = 672
+y_start = 562
 
+# Una vez sabemos en que pixel se cree que esta, definir en que
+# celdilla se encuentra.
+start_cell = find_cell(free_cells, x_start, y_start, cell_x, cell_y)
+fill_cell(map_erosion, start_cell[0], start_cell[1], cell_x, cell_y, 210)
+
+# Una vez localizado al robot en la celdilla se procede a crear el path
+# para recorrer todas las celdillas de la casa.
+# Prioridad para avanzar, Norte, Este, Sur, Oeste
+
+# Diferenciar en el mapa los Obstaculos(0), Obstaculos virtuales(celdas ya visitadas por el robot,c=210),
+# Puntos de Retorno(Puntos de inicio para comenzar a limpiar de nuevo, 240) y Puntos Criticos(Puntos donde
+# el robot se queda atascado entre obstaculos y obstaculos virtuales).
+
+# Para trazar el path se empleara el algoritmo de busqueda en coste uniforme.
+# Los puntos de retorno serám comprobados mientras que el robot se encuentra en movimiento. 
+# Las celdas vacias se clasificaran como puntos de retorno. La lista de los puntos ha de ser actualizda continuamente.
+
+# Los puntos criticos seran clasificados cuando el robot este atascado entre obstaculos o obstaculos virtuales y no se pueda mover.
+
+# Para comprbar la llegada de la celda se consideraran varios offsets de errores en el mundo real. La llegada del robot deberia
+# ser considerada con un pequeño margen de error, de la otra manera el robot comenzaria a oscilar.
+
+# Algoritmo BSA
+# Ira fuera del bucle while
+# Obtener las celdillas vecinas de la celdilla actual y abvanzar al norte si es posible.
+# neighbors = get_neighbors_cells(free_cells, start_cell, cell_x, cell_y)
+# print("VECINOS: ", neighbors)
+# fill_cell(map_erosion, neighbors[0][0][0], neighbors[0][0][1], cell_x, cell_y, 210)
+
+# Crear una ventana de visualización
+cv2.namedWindow('MAP IN REAL TIME', cv2.WINDOW_NORMAL)
+
+return_points = []
+visited_cells = [start_cell] # Lista para guardar celdillas visitadas
+
+current_cell = start_cell
+# Seguir avanzando mientras hayan celdillas sin limpiar
+while len(free_cells) != 0:
+    
+    neighbors = get_neighbors_cells(free_cells, current_cell, cell_x, cell_y)
+
+    # El primer vecino que haya es el movimiento que se va a realizar, el resto son
+    # considerados puntos de retorno. Si no hay vecinos nos encontramos en un punto critico
+    # y se procede a buscar el punto de retorno mas cercano.
+    
+    if len(neighbors) != 0:
+        current_cell = neighbors[0][0]
+        free_cells.remove(current_cell)
+        visited_cells.append(current_cell)
+        neighbors.pop(0)
+        # Si la celda actual en la que estoy era un vecino, se actualiza la lista de puntos de retorno
+        if current_cell in return_points:
+            return_points.remove(current_cell)
+        fill_cell(map_erosion, current_cell[0], current_cell[1], cell_x, cell_y, color_visited)
         
-cv2.imshow('EROSIONED MAP', map_erosion)
+        for cell_ngh in neighbors:
+            if cell_ngh not in return_points:
+                return_points = return_points + [cell_ngh[0]]
+                fill_cell(map_erosion, cell_ngh[0][0], cell_ngh[0][1], cell_x, cell_y, color_retrpnts)
+    else:
+        print("SALGO")
+        fill_cell(map_erosion, current_cell[0], current_cell[1], cell_x, cell_y, 0)
+        # Se procederia a buscar un punto de retorno donde volver a empezar
+    
+    cv2.imshow('MAP IN REAL TIME', map_erosion)
+    cv2.waitKey(10) # 80 buen tiempo
+    
+
+
+'''
+directions = ["North", "East", "South", "West"]
+return_points = util.PriorityQueue() # Cola para guardar celdillas
+pathtocell = util.PriorityQueue() # Cola para guardar los caminos a los nodos
+visited_cells = [] # Lista para guardar celdillas visitadas
+neighbors = [] # get_neighbors_cells(map_erosion, free_cells, start_cell, cell_x, cell_y) # Vecinos del nodo
+path = []
+cost = []                           #   Coste del camino a la celdilla
+
+# Si no hay ninguna direccion, es posible que sea un punto critico o que ya ha terminado de limpiar.        
+return_points.push(start_cell, 0)
+while not return_points.isEmpty():
+        current_cell = return_points.pop()
+        
+        if current_cell not in visited_cells:
+            # if problem.isGoalState(current_cell):
+            #     break
+                
+            visited_cells.append(current_cell)
+            neighbors = get_neighbors_cells(map_erosion, free_cells, start_cell, cell_x, cell_y)
+
+            for child in neighbors:
+                #   Guardo las acciones y el coste para llegar al hijo
+                temp_pth = path + [child[1]]
+                # cost = problem.getCostOfActions(path) + neighbors[2]
+
+                #   Guardo el camino al hijo
+                pathtocell.push(temp_pth, cost)
+                return_points.push(child[0], cost)
+
+        path = pathtocell.pop()
+'''
+
+# cv2.imshow('ORIGINAL MAP', map_img)
 
 cv2.waitKey(0) # La imagen se sigue mostrando segun el parametro que se pase, si es 0 se muestra de forma infinita
 
